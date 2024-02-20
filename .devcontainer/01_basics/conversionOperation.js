7d43dbbffc96987cde0f979c368bea73a3be6a59
SanketDeshmukh29 <109226904+SanketDeshmukh29@users.noreply.github.com>
2024-02-20 19:46:42 +0000

    Datatype conversion in javaScript

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc";
+
+console.log(typeof score);
+
+let valInNumber = Number(score);
+console.log(typeof valInNumber);
+
+console.log(valInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 & false=> 0
+
+let isLoggedIn =1;
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof bolleanIsLoggedIn);
+console.log( bolleanIsLoggedIn);
+
+let someNumber = 33
+
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber);
+console.log(StringNumber);
+
+
+
+
