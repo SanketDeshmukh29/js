b81b58e3c96942edbac905a9e3303544b8b91d24
SanketDeshmukh29 <109226904+SanketDeshmukh29@users.noreply.github.com>
2024-02-20 19:07:31 +0000

    discussion on data Types

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(2+2) // we are using node js not browser 
+
+
+
+
+//number => 2 to power of 53
+let age = 21
+
+//bigint
+let BigInt =2
+console.log(BigInt);
+
+//string =>""
+let name = "sanket"
+
+//Boolean=> true/false
+let isLoggedIn = true
+
+//null =>standalone value 
+let state =null
+console.log(state);
+
+// undefined =>
+let Password;
+
+console.log(typeof age)         // type of tell variables data type
+console.log(typeof Password) // type=>udefined 
+
+console.log(typeof null) // type=> object
